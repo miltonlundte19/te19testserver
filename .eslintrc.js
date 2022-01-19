8fd1a9947ee7646eb0b0a27c29a3b5228881c661
miltonlundte19 <milton.lund@elev.ga.ntig.se>
2022-01-19 13:40:15 +0100

    🎉 initualaist

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    // "indent": ["error", 4],
+    semi: ['error', 'always'] // fel om det saknas semikolon
+  }
+};
