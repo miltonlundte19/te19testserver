0349913c479f0f2991050810acf3907afaeb61c8
miltonlundte19 <milton.lund@elev.ga.ntig.se>
2022-02-25 11:18:15 +0100

    ✨ lade in botstrap och ladde till en poster från usern

--- routes/tasks.js
+++ routes/tasks.js
@@ -5,14 +5,53 @@ const pool = require('../database');
 router.get('/', async (req, res, next) => {
     await pool
         .promise()
-        .query('SELECT * FROM tasks')
+        .query('SELECT * FROM tasks ORDER BY updated_at DESC')
         .then(([rows, fields]) => {
-            console.log(rows);
             res.render('tasks.njk', {
                 tasks: rows,
                 title: 'Tasks',
                 layout: 'layout.njk'
             });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json({
+                task: {
+                    errpr: 'Errpr gettomg tasls'
+                }
+            });
+        });
+});
+
+router.post('/', async (req, res, next) => {
+    const task = req.body.task;
+
+    if (task.length < 3) {
+        // res.render('tasks.njk', {
+        //    tasks: {},
+        //    title: 'Tasks',
+        //    layout: 'layout.njk'
+        // });
+        res.status(400).json({
+            task: {
+                error: 'Bad request'
+            }
+        });
+    }
+    await pool
+        .promise()
+        .query('INSERT INTO tasks (task) VALUES (?)', [task])
+        .then((respons) => {
+            console.log(respons[0].affectedRows);
+            if (respons[0].affectedRows === 1) {
+                res.redirect('/tasks');
+            } else {
+                res.status(400).json({
+                    task: {
+                        error: 'Bad request'
+                    }
+                });
+            }
         })
         .catch((err) => {
             console.log(err);
